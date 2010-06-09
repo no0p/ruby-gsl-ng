bd233fe96f348ee3e4c8b59a340420b59613f006
v01d <phreakuencies@gmail.com>
2010-06-08 22:25:50 -0300

    Use -O4 for compiling. This thing must go fast...

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 require 'mkmf'
 gsl_vars = pkg_config('gsl') or raise 'GSL not found!'
-extra_flags='-O3'
-with_cppflags("#{$CPPFLAGS} #{extra_flags}") { true }
+extra_flags='-O4 -DHAVE_INLINE'
+with_cppflags("#{extra_flags}") { true }
 create_makefile('gslng_extensions')
