ed988f3e29c580d34c74173f9a3f39082325868c
v01d <phreakuencies@gmail.com>
2010-07-08 23:51:45 -0300

    Fixed (hopefully) issue #2

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -6,6 +6,9 @@ extra_flags='-O4 -DHAVE_INLINE=1'
 ## Check for GSL
 gsl_vars = pkg_config('gsl') or raise 'GSL not found!'
 
+# Configuration
+if (RUBY_VERSION =~ /^1\.8/) then $libs << ' -lstdc++' end # Seems necessary in some cases
+
 ## Create Makefile
 with_cppflags("#{extra_flags}") { true }
 create_makefile('gslng_extensions')
