d7f04780b1c07db4bc23e93a3616fa4e8d1f2105
v01d <phreakuencies@gmail.com>
2010-09-24 11:47:52 -0300

    Fix for #3

--- ext/plotting.cpp
+++ ext/plotting.cpp
@@ -4,6 +4,6 @@
 extern "C" int gsl_matrix_putdata(gsl_matrix* m, int fd) {
   size_t bytes = m->size1 * m->size2 * sizeof(double);
   long ret = write(fd, m->data, bytes);
-  if (ret == -1 || (ulong)ret < bytes) return errno;
+  if (ret == -1 || (unsigned long)ret < bytes) return errno;
   else return 0;
 }
