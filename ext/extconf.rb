6c48a0e571731f007dc358ff2cc337e4d88333fb
v01d <phreakuencies@gmail.com>
2010-09-30 20:26:08 -0300

    Added check for pkg-config, to avoid future problems

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,6 +4,7 @@ require 'mkmf'
 extra_flags='-O4 -DHAVE_INLINE=1'
 
 ## Check for GSL
+find_executable('pkg-config') or raise 'pkg-config should be installed'
 gsl_vars = pkg_config('gsl') or raise 'GSL not found!'
 
 # Configuration
